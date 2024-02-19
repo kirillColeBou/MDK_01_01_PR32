3958fdf018fea36891bc1fab989d21c3440fb486
subbotin.59 <gsubbotin.2019@gmail.com>
2024-02-19 15:56:12 +0500

    Разработанный интерфейс MainWindow. Тепляков

--- VinylRecordsApplication_Тепляков/MainWindow.xaml.cs
+++ VinylRecordsApplication_Тепляков/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace VinylRecordsApplication_Тепляков
         {
             InitializeComponent();
         }
+
+        private void OpenRecordList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenRecordAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportRecord(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenManufacturersList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenManufacturersAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenSupplyList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenSupplyAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenStateList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenStateAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

4292804d274df4466126fe8f41fcd67ca18f15b5
kirillColeBou <kirillteplycov888@gmail.com>
2024-03-19 23:22:07 +0500

    Разработка страницы для добавления и изменения поставщика. Тепляков

--- VinylRecordsApplication_Тепляков/Pages/Manufacturer/Elements/Manufacturer.xaml.cs
+++ VinylRecordsApplication_Тепляков/Pages/Manufacturer/Elements/Manufacturer.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VinylRecordsApplication_Тепляков.Classes;
 
 namespace VinylRecordsApplication_Тепляков.Pages.Manufacturer.Elements
 {
@@ -20,9 +21,37 @@ namespace VinylRecordsApplication_Тепляков.Pages.Manufacturer.Elements
     /// </summary>
     public partial class Manufacturer : UserControl
     {
-        public Manufacturer()
+        IEnumerable<Classes.Country> Countries = Country.AllCountries();
+        Pages.Manufacturer.Main main;
+        Classes.Manufacturer manufacturer;
+        public Manufacturer(Classes.Manufacturer manufacturer, Pages.Manufacturer.Main main)
         {
             InitializeComponent();
+            tbName.Text = manufacturer.Name;
+            tbCountry.Text = Countries.Where(x => x.Id == manufacturer.CountryCode).First().Name;
+            tbPhone.Text = manufacturer.Phone.ToString();
+            tbEmail.Text = manufacturer.Mail;
+            this.manufacturer = manufacturer;
+            this.main = main;
+        }
+
+        private void EditManufacturer(object sender, RoutedEventArgs e) => MainWindow.mainWindow.OpenPage(new Pages.Manufacturer.Add(this.manufacturer));
+
+        private void DeleteManufacturer(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Удалить поставщика: {this.manufacturer.Name}?", "Уведомление", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                if (Classes.Record.AllRecords().Count(x => x.IdManufacturer == manufacturer.Id) > 0)
+                {
+                    MessageBox.Show($"Поставщика {this.manufacturer.Name} невозможно удалить. Для начала удалите зависимости.", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    this.manufacturer.Delete();
+                    main.manufacturerParent.Children.Remove(this);
+                    MessageBox.Show($"Поставщик {this.manufacturer.Name} успешно удалена.", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
         }
     }
 }

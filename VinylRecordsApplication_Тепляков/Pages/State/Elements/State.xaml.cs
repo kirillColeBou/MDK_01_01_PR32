7d9f428455e8289c3ee4e8ce5714d0375c1aead8
kirillColeBou <kirillteplycov888@gmail.com>
2024-03-19 22:10:55 +0500

    Разработанная страница State. Тепляков

---
+++ VinylRecordsApplication_Тепляков/Pages/State/Elements/State.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VinylRecordsApplication_Тепляков.Pages.State.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для State.xaml
+    /// </summary>
+    public partial class State : UserControl
+    {
+        Classes.State state;
+        Pages.State.Main main;
+        public State(Classes.State state, Pages.State.Main main)
+        {
+            InitializeComponent();
+            this.state = state;
+            this.main = main;
+            tbName.Text = this.state.Name;
+            tbSubname.Text = this.state.Subname;
+            tbDescription.Text = this.state.Description;
+        }
+
+        private void EditState(object sender, RoutedEventArgs e) => MainWindow.mainWindow.OpenPage(new Pages.State.Add(state));
+
+        private void DeleteState(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Удалить состояние: {this.state.Name}?", "Уведомление", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                IEnumerable<Classes.Record> allRecords = Classes.Record.AllRecords();
+                if (allRecords.Where(x => x.IdState == state.Id).Count() > 0)
+                {
+                    MessageBox.Show($"Состояние {this.state.Name} невозможно удалить. Для начала удалите зависимости.", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    this.state.Delete();
+                    main.stateParent.Children.Remove(this);
+                    MessageBox.Show($"Состояние {this.state.Name} успешно удалена.", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+    }
+}
